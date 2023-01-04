972b3e66dd22931453c469fb3c389997c5027b27
Alulestad <vladimirlestad@gmail.com>
2023-01-03 19:41:53 -0500

    taller17

---
+++ src/main/java/com/example/demo/ProyectoPaU2DllApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPaU2DllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPaU2DllApplication.class, args);
+	}
+
+}
